0a92746fb4ff754965741f10b2b70f912e7b5467
Amrutha <cloudshell-user@ip-10-144-103-40.ec2.internal>
2025-10-07 16:59:44 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, Amrutha! Your Node.js app is running 🎉');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
